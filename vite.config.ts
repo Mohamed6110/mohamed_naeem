82cf9ff5e867d3fa993ee03f1c3f86f4219d522f
Mohammed Naeem <82940620+Mohamed6110@users.noreply.github.com>
2026-02-16 19:15:11 +0200

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/mohammed-naeem-fawzy-portfolio/',
+      base: '/mohammed_naeem/',
       server: {
         port: 3000,
         host: '0.0.0.0',
